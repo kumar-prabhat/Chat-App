c6225f6743ad9326140080cc7221b64e5ebfaa88
Prabhat Kumar <prabhatkumar1791995@gmail.com>
2020-05-15 23:35:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(4000, function () {
+  console.log('Listening to requests on port 4000');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function (socket) {
+  socket.on('chat', function (data) {
+    io.sockets.emit('chat', data);
+  });
+
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
+  });
+});
